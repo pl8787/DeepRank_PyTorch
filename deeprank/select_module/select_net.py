c4f00bca39b46691c5c12bb1198af18d9f39b18a
pl8787 <pl8787@gmail.com>
2019-03-31 22:30:06 -0400

    data adapter draft

--- deeprank/select_module/select_net.py
+++ deeprank/select_module/select_net.py
@@ -10,6 +10,8 @@ class SelectNet(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
+        # S: sentence, L: list, LL: list of list
+        self.output_type = 'S'
 
     def forward(self, q_data, d_data, q_len, d_len):
         return q_data, d_data, q_len, d_len
